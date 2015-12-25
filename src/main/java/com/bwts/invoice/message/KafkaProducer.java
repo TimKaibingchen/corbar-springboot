ed0e80dea4a0c8ffa19f071c61ec5704a95764df
TimChen <tch@tradeshift.com>
2015-12-25 18:47:23 +0800

    Added key for producer

--- src/main/java/com/bwts/invoice/message/KafkaProducer.java
+++ src/main/java/com/bwts/invoice/message/KafkaProducer.java
@@ -13,28 +13,30 @@ import kafka.producer.ProducerConfig;
 
 @Component
 public class KafkaProducer implements DisposableBean {
-    
+
     private final Producer producer;
     private final String topic;
-    
+    long i = 0;
+
     @Autowired
     public KafkaProducer(
             @Value("${producer.zookeeper.connect}") String url,
             @Value("${producer.topic}") String topic,
             @Value("${producer.broker.list}") String brokerList) {
-        
+
         Properties producerProps = new Properties();
         producerProps.put("serializer.class", "kafka.serializer.StringEncoder");
         producerProps.put("zk.connect", url);
         producerProps.put("metadata.broker.list", brokerList);
-        
+
         producer = new Producer<String, String>(new ProducerConfig(producerProps));
         this.topic = topic;
     }
-    
+
     public void send(String message) {
-        KeyedMessage<String, String> keyedMessage = new KeyedMessage<String, String>(topic, message);
+        KeyedMessage<String, String> keyedMessage = new KeyedMessage<String, String>(topic, Long.toString(i), message);
         producer.send(keyedMessage);
+        i++;
     }
 
     @Override
