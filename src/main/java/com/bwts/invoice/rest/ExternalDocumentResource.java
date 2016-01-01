b2198e000bfbdd322774bf4cfb68f23a0d196549
Yao Hu <huy@tradeshift.com>
2016-01-01 14:41:53 +0800

    deprecate old kafka producer, use new one in backend-common

--- src/main/java/com/bwts/invoice/rest/ExternalDocumentResource.java
+++ src/main/java/com/bwts/invoice/rest/ExternalDocumentResource.java
@@ -1,44 +1,47 @@
 package com.bwts.invoice.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
+import com.bwts.common.exception.APIException;
+import com.bwts.common.exception.ErrorCode;
+import com.bwts.common.invoice.InvoiceMessage;
+import com.bwts.common.message.KafkaMessageProducer;
+import com.bwts.common.security.DefaultUsers;
+import com.bwts.common.security.UserContext;
+import com.bwts.common.security.UserContextHolder;
+import com.bwts.invoice.service.ThirdPartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import com.bwts.common.exception.APIException;
-import com.bwts.common.exception.ErrorCode;
-import com.bwts.invoice.message.KafkaProducer;
-import com.bwts.invoice.service.ThirdPartyService;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Component
 @Path("external/documents")
 public class ExternalDocumentResource {
-    
-    private final KafkaProducer kafkaProducer;
 
     private final ThirdPartyService thirdPartyService;
 
     @Value("${access.token.enabled}") private boolean tokenEnabled;
 
-    @Autowired
-    public ExternalDocumentResource(KafkaProducer kafkaProducer, ThirdPartyService thirdPartyService) {
-        this.kafkaProducer = kafkaProducer;
-        this.thirdPartyService = thirdPartyService;
+    private final KafkaMessageProducer messageProducer;
 
+    private final String invoiceTopic;
+
+    @Autowired
+    public ExternalDocumentResource(ThirdPartyService thirdPartyService,
+            KafkaMessageProducer producer,
+            @Value("${consumer.invoice.topic}") String topic) {
+        this.thirdPartyService = thirdPartyService;
+        this.messageProducer = producer;
+        this.invoiceTopic = topic;
     }
     
     @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Response createDocument(String message, @HeaderParam("BWTS-TOKEN") String token) {
+    public Response createDocument(String message, @HeaderParam("BWTS-TOKEN") String token) throws APIException {
         if (tokenEnabled) {
             if (token == null) {
                 throw new APIException(HttpStatus.FORBIDDEN, ErrorCode.NO_TOKEN, "no bwts-token in header");
@@ -51,7 +54,10 @@ public class ExternalDocumentResource {
     }
 
     private Response sendKafkaMessage(String message) {
-        kafkaProducer.send(message);
+        UserContextHolder.setContext(new UserContext(DefaultUsers.SYSTEM));
+        InvoiceMessage invoiceMessage = new InvoiceMessage.InvoiceMessgeBuilder().withInvoiceData(message).build();
+        messageProducer.send(invoiceTopic,invoiceMessage);
+
         return Response.status(Response.Status.ACCEPTED).build();
     }
 }
