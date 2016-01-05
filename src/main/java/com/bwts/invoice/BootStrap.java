a4fb6f38ce75351ca5c5c4a9f4495136de4eac31
Yao Hu <huy@tradeshift.com>
2016-01-05 13:39:41 +0800

    upgrade to backend_common 0.1.7

--- src/main/java/com/bwts/invoice/BootStrap.java
+++ src/main/java/com/bwts/invoice/BootStrap.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan("com.bwts.common,com.bwts.invoice")
+@ComponentScan("com.bwts.common.kafka.producer,com.bwts.invoice")
 public class BootStrap extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BootStrap.class);
